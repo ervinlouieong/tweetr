7988f3422b73dba9ad0d73508746656f63e7dd1d
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-17 21:21:13 +0000

    Add form submission using JQuery and fetch tweets using Ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,56 +1,27 @@
 $(function() {
-// Fake data taken from tweets.json
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+
+  function loadTweets() { 
+    $.ajax({
+      url: "/tweets",
+      method: "GET"
+    }).done(function(data) {
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
+
+let $submitTweet = $(".submitTweet");
+$submitTweet.on("submit", function(event) {
+  event.preventDefault();
+  $(this).serialize();
+});
+
 
 function renderTweets(tweets) {
-  let $tweetsContainer = $('#tweetsContainer');
-  for (let element in data) {
-    let tweet = data[element];
+  let $tweetsContainer = $("#tweetsContainer");
+  for (let element in tweets) {
+    let tweet = tweets[element];
     $tweetsContainer.append(createTweetElement(tweet));
   }
 }
@@ -65,16 +36,15 @@ function createTweetElement(tweet) {
   let $footer = $("<footer>");
   let $dateCreated = $("<span>").addClass("dateCreated").text(tweet.created_at);
   let $icons = $("<div>").addClass("icons");
-  $icons.append( `<i class="fa fa-flag" aria-hidden="true"></i>
-                 <i class="fa fa-retweet" aria-hidden="true"></i>
-                 <i class="fa fa-heart" aria-hidden="true"></i>)`);
   $tweet.append($header, $tweetContent, $footer);
   $header.append($userAvatar, $userNAme, $userHandle);
   $footer.append($dateCreated, $icons);
-
+  $icons.append( `<i class="fa fa-flag" aria-hidden="true"></i>
+                 <i class="fa fa-retweet" aria-hidden="true"></i>
+                 <i class="fa fa-heart" aria-hidden="true"></i>)`);
   return $tweet;
 }
 
 
-  renderTweets(data);
+  //renderTweets(data);
 });
