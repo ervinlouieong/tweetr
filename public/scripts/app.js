055cc75672384a19f846ea8ea5d908b1798b4255
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-18 05:03:53 +0000

    Added getDaysDifference function to show days/minutes elapsed when tweets was created

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,74 +1,96 @@
 $(function() {
 
+// Use to convert Date.now()
+  function getDaysDifference(tweet) {
+    let $dateDifference = 0;
+    const minutes = 1000 * 60;
+    const hours = minutes * 60;
+    const days = hours * 24;
+    let dateDiff = Math.round((Date.now() - tweet.created_at) / days);
+    if (dateDiff === 0) {
+      dateDiff = Math.round((Date.now() - tweet.created_at) / hours);
+      if (dateDiff === 0) {
+        dateDiff = Math.round((Date.now() - tweet.created_at) / minutes);
+        $dateDifference = $("<span>").addClass("dateCreated").text(dateDiff + " minutes ago");
+      } else {
+        $dateDifference = $("<span>").addClass("dateCreated").text(dateDiff + " hours ago");
+      }
+    } else {
+      $dateDifference = $("<span>").addClass("dateCreated").text(dateDiff + " days ago");
+    }
+    return $dateDifference;
+  }
+
 // Create New Tweets, the inside of section #tweetsContainer.
-function createTweetElement(tweet) {
-  let $tweet = $("<article>").addClass("tweet");
-  let $header = $("<header>");
-  let $userAvatar = $("<img>").addClass("userAvatar").attr("src", tweet.user.avatars.small);
-  let $userNAme = $("<h2>").addClass("userName").text(tweet.user.name);
-  let $userHandle = $("<h6>").addClass("userHandle").text(tweet.user.handle);
-  let $tweetContent = $("<p>").addClass("tweetText").text(tweet.content.text);
-  let $footer = $("<footer>");
-  let $dateCreated = $("<span>").addClass("dateCreated").text(tweet.created_at);
-  let $icons = $("<div>").addClass("icons");
-  $tweet.append($header, $tweetContent, $footer);
-  $header.append($userAvatar, $userNAme, $userHandle);
-  $footer.append($dateCreated, $icons);
-  $icons.append( `<i class="fa fa-flag" aria-hidden="true"></i>
+  function createTweetElement(tweet) {
+    let $tweet = $("<article>").addClass("tweet");
+    let $header = $("<header>");
+    let $userAvatar = $("<img>").addClass("userAvatar").attr("src", tweet.user.avatars.small);
+    let $userNAme = $("<h2>").addClass("userName").text(tweet.user.name);
+    let $userHandle = $("<h6>").addClass("userHandle").text(tweet.user.handle);
+    let $tweetContent = $("<p>").addClass("tweetText").text(tweet.content.text);
+    let $footer = $("<footer>");
+    let $icons = $("<div>").addClass("icons");
+
+    $tweet.append($header, $tweetContent, $footer);
+    $header.append($userAvatar, $userNAme, $userHandle);
+    $footer.append(getDaysDifference(tweet), $icons);
+    $icons.append( `<i class="fa fa-flag" aria-hidden="true"></i>
                  <i class="fa fa-retweet" aria-hidden="true"></i>
-                 <i class="fa fa-heart" aria-hidden="true"></i>)`);
-  return $tweet;
-}
+                 <i class="fa fa-heart" aria-hidden="true"></i>`);
+    return $tweet;
+  }
 
 // Iterate each entry on /tweets. Last entry is on top.
-function renderTweets(tweets) {
-  let $tweetsContainer = $("#tweetsContainer");
-  $tweetsContainer.empty();
-  for (let element in tweets) {
-    let tweet = tweets[element];
-    $tweetsContainer.prepend(createTweetElement(tweet));
+  function renderTweets(tweets) {
+    let $tweetsContainer = $("#tweetsContainer");
+    $tweetsContainer.empty();
+    for (let element in tweets) {
+      let tweet = tweets[element];
+      $tweetsContainer.prepend(createTweetElement(tweet));
+    }
   }
-}
-
-// On Submit Tweet
-$(".submitTweet").on("submit", function(event) {
-  event.preventDefault();
-  // Form validation of textarea
-  let textLength = $("textarea").val().length;
-  if (textLength === 0) {
-    alert("Please fill in the tweetbox.");
-  } else if (textLength > 140) {
-    alert("Character Limit Exceeded!");
-  } else {
-    // Add the value to /tweets 
+  
+// Get all the values from /tweets
+  function loadTweets() {
     $.ajax({
       url: "/tweets",
-      method: "POST",
-      data: $(this).serialize()
+      method: "GET"
     }).done(function(data) {
-      loadTweets();
-    })
-  }
-});
-
-// Get all the values from /tweets
-function loadTweets() { 
-  $.ajax({
-    url: "/tweets",
-    method: "GET"
-  }).done(function(data) {
-     renderTweets(data); 
+      renderTweets(data);
     });
-}
+  }
+  loadTweets();
 
-loadTweets();
+// On Submit Tweet
+  $(".submitTweet").on("submit", function(event) {
+    event.preventDefault();
+// Form validation of textarea
+    let textLength = $("textarea").val().length;
+    if (textLength === 0) {
+      alert("Please fill in the tweetbox.");
+    } else if (textLength > 140) {
+      alert("Character Limit Exceeded!");
+    } else {
+// Add the value to /tweets
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $(this).serialize()
+      }).done(function(data) {
+        loadTweets();
+      });
+    }
+  });
+
+
 
 // Compose button
-$("button").on("click", function(event) {
-  event.preventDefault();
-  $(".new-tweet").slideToggle("slow", function(){
-        $("textarea").focus();
+  $("button").on("click", function(event) {
+    event.preventDefault();
+    $(".new-tweet").slideToggle("slow", function(){
+      $("textarea").focus();
+    });
   });
-});
   
 });
